6f9787288d02f86d2fd1fe49383d6343ebfbb5e9
Nevin John <nevinj21@gmail.com>
2026-01-13 21:27:28 +0530

    Assignment 1 - Typescript

---
+++ typescript-training/assignments/assignment1.ts
@@ -0,0 +1,81 @@
+// 1. Temperature of a city in degrees Celsius: 25.5
+let temp : number =  25.5;
+console.log("Temperature : "+temp);
+
+//2. Whether a customer has placed an order : true or false
+let orderPlaced : boolean = true;
+console.log("Order Placed : "+orderPlaced);
+
+//3. Person's phone number: "123-456-7890"
+let phoneNumber : string = "123-456-7890";
+console.log("Phone Number : " +phoneNumber);
+
+//4. Amount of money in a customer's bank account: 1000.50
+let bankBalance : number = 1000.50;
+console.log("Bank Balance : "+bankBalance);
+
+//5. Person's email address: "john.doe@example.com"
+let emailAddress : string = "john.doe@example.com";
+console.log("email address : "+emailAddress);
+
+//6. Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+let coordinates : [number,number] = [37.7749, -122.4194];
+console.log("coordinates : "+coordinates);
+
+// 7. Person's marital status: true or false
+let maritalStatus : boolean = true;
+console.log("Marital Status : "+maritalStatus);
+
+// 8. Person's occupation: "Software Engineer"
+let occupation : string = "Software Engineer";
+console.log("Occupation : "+occupation);
+
+// 9. Person's favourite colour: "Blue"
+let favColor : string = "Blue";
+console.log("Persons favourite color : "+favColor);
+
+// 10.Current year: 2023
+let currrentYear : number = 2026
+console.log("Current Year : "+currrentYear)
+
+// 11.Number of followers on a social media platform: 1,000,000
+let followersCount : string = "1,000,000";
+console.log("Followers Count : "+followersCount);
+
+// 12.Rating of a movie: 7.5
+let rating : number = 7.5;
+console.log("Rating : "+rating);
+
+// 13.Person's blood type: 'A'
+let bloodType : string = 'A'
+console.log("Blood Group : "+bloodType);
+
+//14.Title of a book: "To Kill a Mockingbird"
+let titleBook : string = 'To Kill a Mockingbird';
+console.log("Title of Book : "+titleBook);
+
+//15.Number of employees in a company: 500
+let numberOfEmpsCompany : number = 500;
+console.log("Number of Employees in Company : "+numberOfEmpsCompany);
+
+// 16.Time of an event: 2:30 PM
+let timeEvent : string = '2:30 PM';
+console.log("Time of an Event : "+timeEvent);
+
+// 17.Name of a country: "United States"
+let countryName : string = 'USA';
+console.log("Name of a country : "+countryName);
+
+
+// 18.Person's eye color: "Brown"
+let personEyeColor :string = "Brown";
+console.log("Person's Eye Color : "+personEyeColor);
+
+
+// 19.Person's birthplace: "New York City"
+let birthplace : string = "New York City";
+console.log("Person's Birth Place : "+birthplace);
+
+// 20. Distance between two cities: 200.5
+let dist :number = 200.5;
+console.log("Distance between two Cities : "+dist)
