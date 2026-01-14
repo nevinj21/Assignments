970e72b76b443cd10f9c8f75d72b44310b091da8
Nevin John <nevinj21@gmail.com>
2026-01-14 23:11:26 +0530

    Assignment 3 - BankLoan

---
+++ typescript-training/assignments/assignment3.ts
@@ -0,0 +1,37 @@
+function bank(
+    customerName: string,
+    creditScore: number,
+    income: number,
+    isEmployed: boolean,
+    debtToIncomeRatio: number
+): void {
+    if (creditScore >= 750) {
+        console.log("Loan is approved");
+    }
+    else if (creditScore >= 650 && creditScore < 750) {
+        console.log("Needed additional checks");
+        if (income >= 50000) {
+            console.log("Loan is considered for "+customerName)
+            if (isEmployed == true) {
+                if (debtToIncomeRatio < 40) {
+                    console.log("Congrats! Loan is Approved for "+customerName)
+                }
+                else {
+                    console.log("Sorry Loan is rejected for "+customerName +" since debtToIncomeRatio is higher")
+                }
+            }
+            else {
+                console.log("Sorry Loan is rejected for "+customerName+ " since customer is unemployed")
+            }
+        }
+        else
+        {
+            console.log("Sorry Loan is rejected for " +customerName+" since income is less")
+        }
+    }
+    else {
+        console.log("Loan is rejected since cibil score is Low");
+    }
+}
+
+bank("John Doe", 740, 55000.0, false, 35.0);
