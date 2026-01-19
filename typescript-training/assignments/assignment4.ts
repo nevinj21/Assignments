0668d4c6c730f6cb276ad7baab35deb0b0c33755
Nevin John <nevinj21@gmail.com>
2026-01-19 10:27:37 +0530

    Assignment 4 - Added Credit and Debit Transaction Count

--- typescript-training/assignments/assignment4.ts
+++ typescript-training/assignments/assignment4.ts
@@ -1,6 +1,8 @@
 function Transaction(transactionAmount: number[]): void {
     let credits = 0;
     let debits = 0;
+    let creditCount = 0;
+    let debitCount = 0;
     let totalAmount = 0;
     let suspiciousTransfer = 0;
     for (let i = 0; i < transactionAmount.length; i++) {
@@ -8,9 +10,11 @@ function Transaction(transactionAmount: number[]): void {
 
         if (value != undefined && value > 0) {
             credits = credits + value;
+            creditCount++;
         }
         if (value != undefined && value < 0) {
             debits = debits + value;
+            debitCount++;
         }
         if ((value != undefined && value > 10000) || (value != undefined && value < -10000))
         {
@@ -21,6 +25,8 @@ function Transaction(transactionAmount: number[]): void {
     totalAmount = debits + credits;
     console.log(`Total number of Suspicious Transactions ${suspiciousTransfer}`)
     console.log(`Total number of Debit and Credit Transactions Completed : ${transactionAmount.length}`);
+    console.log(`Total Number of Credit Transactions ${creditCount}`);
+    console.log(`Total Number of Debit Transactions ${debitCount}`)
     console.log(`Total amount credited and debited in account is Credit : ${credits}, Debit : ${debits}`);
     console.log(`Total amount left in bank account :  ${totalAmount}`)
 }
